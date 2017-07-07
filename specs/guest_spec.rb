89da55ef502c3ece82e00bf4f830bfd3349ebfd7
Chris Hunter <orkem2@gmail.com>
2017-07-07 13:43:08 +0100

    Created Guest class and tested that an instance is a Guest object

---
+++ specs/guest_spec.rb
@@ -0,0 +1,25 @@
+require('minitest/autorun')
+require('pry')
+
+require_relative('../room.rb')
+require_relative('../song.rb')
+require_relative('../guest.rb')
+
+
+class TestGuest < MiniTest::Test
+  def setup()
+    @guest1 = Guest.new('Chris')
+  end
+  def test_is_guest?
+    assert_equal(@guest1.class, Guest.new('John').class)
+  end
+
+
+
+
+
+
+
+
+
+end
---
+++ specs/song_spec.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('pry')
+
+require_relative('../room.rb')
+require_relative('../song.rb')
+require_relative('../guest.rb')
+
+
+class TestSong < MiniTest::Test
+
+
+
+
+
+
+
+
+
+
+
+
+end
