d37a2ec716c16b0b77faa81b9edd791030cbf61b
Chris Hunter <orkem2@gmail.com>
2017-07-07 13:57:55 +0100

    Added add_guest() to Room and passed test

--- room.rb
+++ room.rb
@@ -1,6 +1,10 @@
 class Room
-
+  attr_reader :guests
   def initialize(guests)
     @guests = guests
+  end
+
+  def add_guest(guest)
+    @guests <<  guest
   end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -20,6 +20,14 @@ class TestRoom < MiniTest::Test
     assert_equal(@room1.class, Room.new(@guests).class)
   end
 
+  def test_add_guest
+    old_count = @room1.guests.count
+    new_guest = Guest.new("John")
+    @room1.add_guest(new_guest)
+    assert_equal(old_count + 1, @room1.guests.count)
+
+  end
+
 
 
 
